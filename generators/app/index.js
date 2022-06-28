b0e6d5c7c3d1b134faa4f7cfa66387d6bd5ef2ca
dsancalm <88796552+dsancalm@users.noreply.github.com>
2022-06-28 10:00:47 +0200

    controlador y enrutador
    añadido al template el controlador y enrutador

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,7 +11,7 @@ module.exports = class extends Generator {
       )
     );
 
-    this.answers = await this.prompt([
+    this.model = await this.prompt([
       {
         type: "input",
         name: "modelName",
@@ -21,17 +21,24 @@ module.exports = class extends Generator {
         type: "input",
         name: "modelFields",
         message: "Your model body"
+      }
+    ]);
+    this.appConfig = await this.prompt([
+      {
+        type: "input",
+        name: "port",
+        message: "Your app port"
       },
       {
-        type: "confirm",
-        name: "cool",
-        message: "Would you like to enable the Cool feature?"
+        type: "input",
+        name: "mongoUrl",
+        message: "Your mongo url database"
       }
     ]);
   }
 
   writing() {
-    var modelNameFile = this.answers.modelName;
+    var modelNameFile = this.model.modelName;
     this.fs.copyTpl(
       this.templatePath("tsconfig.json.txt"),
       this.destinationPath("tsconfig.json")
@@ -46,17 +53,17 @@ module.exports = class extends Generator {
         "src/dao/repository/" + modelNameFile + "Repository.ts"
       ),
       {
-        modelName: this.answers.modelName,
-        modelFields: this.answers.modelFields,
-        modelVar: this.answers.modelName.toLowerCase()
+        modelName: this.model.modelName,
+        modelFields: this.model.modelFields,
+        modelVar: this.model.modelName.toLowerCase()
       }
     );
     this.fs.copyTpl(
       this.templatePath("interfaces/dao/Dao.ejs"),
       this.destinationPath("src/interfaces/dao/I" + modelNameFile + "Dao.ts"),
       {
-        modelName: this.answers.modelName,
-        modelVar: this.answers.modelName.toLowerCase()
+        modelName: this.model.modelName,
+        modelVar: this.model.modelName.toLowerCase()
       }
     );
 
@@ -64,15 +71,15 @@ module.exports = class extends Generator {
       this.templatePath("dao/DaoImpl.ejs"),
       this.destinationPath("src/dao/" + modelNameFile + "DaoImpl.ts"),
       {
-        modelName: this.answers.modelName,
-        modelVar: this.answers.modelName.toLowerCase()
+        modelName: this.model.modelName,
+        modelVar: this.model.modelName.toLowerCase()
       }
     );
     this.fs.copyTpl(
       this.templatePath("models/entities/Model.ejs"),
-      this.destinationPath("src/models/entities/I" + modelNameFile + ".ts"),
+      this.destinationPath("src/models/entities/" + modelNameFile + ".ts"),
       {
-        modelName: this.answers.modelName
+        modelName: this.model.modelName
       }
     );
     this.fs.copyTpl(
@@ -81,16 +88,52 @@ module.exports = class extends Generator {
         "src/interfaces/service/I" + modelNameFile + "Service.ts"
       ),
       {
-        modelName: this.answers.modelName,
-        modelVar: this.answers.modelName.toLowerCase()
+        modelName: this.model.modelName,
+        modelVar: this.model.modelName.toLowerCase()
       }
     );
     this.fs.copyTpl(
       this.templatePath("service/ServiceImpl.ejs"),
       this.destinationPath("src/service/" + modelNameFile + "ServiceImpl.ts"),
       {
-        modelName: this.answers.modelName,
-        modelVar: this.answers.modelName.toLowerCase()
+        modelName: this.model.modelName,
+        modelVar: this.model.modelName.toLowerCase()
+      }
+    );
+
+    this.fs.copyTpl(
+      this.templatePath("controllers/controller.ejs"),
+      this.destinationPath(
+        "src/controllers/" +
+          modelNameFile +
+          "/" +
+          modelNameFile +
+          "Controller.ts"
+      ),
+      {
+        modelName: this.model.modelName,
+        modelVar: this.model.modelName.toLowerCase()
+      }
+    );
+
+    this.fs.copyTpl(
+      this.templatePath("controllers/router.ejs"),
+      this.destinationPath(
+        "src/controllers/" + modelNameFile + "/" + modelNameFile + "Router.ts"
+      ),
+      {
+        modelName: this.model.modelName,
+        modelVar: this.model.modelName.toLowerCase()
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath("index.ejs"),
+      this.destinationPath("src/index.ts"),
+      {
+        port: this.appConfig.port,
+        mongoUrl: this.appConfig.mongoUrl,
+        modelName: this.model.modelName,
+        modelVar: this.model.modelName.toLowerCase()
       }
     );
   }
