171b47a5882f5d42b0b87feb9c60560946908f57
dsancalm <88796552+dsancalm@users.noreply.github.com>
2022-06-28 09:12:21 +0200

    cambio de formato de .ts a .ejs
    cambio de formato de .ts a .ejs
    +
    añadido del template para la capa de servicios

--- generators/app/index.js
+++ generators/app/index.js
@@ -33,15 +33,15 @@ module.exports = class extends Generator {
   writing() {
     var modelNameFile = this.answers.modelName;
     this.fs.copyTpl(
-      this.templatePath("tsconfig.json"),
+      this.templatePath("tsconfig.json.txt"),
       this.destinationPath("tsconfig.json")
     );
     this.fs.copyTpl(
-      this.templatePath("package.json"),
+      this.templatePath("package.json.txt"),
       this.destinationPath("package.json")
     );
     this.fs.copyTpl(
-      this.templatePath("dao/repository/Repository.ts"),
+      this.templatePath("dao/repository/Repository.ejs"),
       this.destinationPath(
         "src/dao/repository/" + modelNameFile + "Repository.ts"
       ),
@@ -52,7 +52,7 @@ module.exports = class extends Generator {
       }
     );
     this.fs.copyTpl(
-      this.templatePath("interfaces/dao/Dao.ts"),
+      this.templatePath("interfaces/dao/Dao.ejs"),
       this.destinationPath("src/interfaces/dao/I" + modelNameFile + "Dao.ts"),
       {
         modelName: this.answers.modelName,
@@ -61,7 +61,7 @@ module.exports = class extends Generator {
     );
 
     this.fs.copyTpl(
-      this.templatePath("dao/DaoImpl.ts"),
+      this.templatePath("dao/DaoImpl.ejs"),
       this.destinationPath("src/dao/" + modelNameFile + "DaoImpl.ts"),
       {
         modelName: this.answers.modelName,
@@ -69,10 +69,28 @@ module.exports = class extends Generator {
       }
     );
     this.fs.copyTpl(
-      this.templatePath("models/entities/Model.ts"),
-      this.destinationPath("src/models/entities/" + modelNameFile + ".ts"),
+      this.templatePath("models/entities/Model.ejs"),
+      this.destinationPath("src/models/entities/I" + modelNameFile + ".ts"),
       {
         modelName: this.answers.modelName
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath("interfaces/service/Service.ejs"),
+      this.destinationPath(
+        "src/interfaces/service/I" + modelNameFile + "Service.ts"
+      ),
+      {
+        modelName: this.answers.modelName,
+        modelVar: this.answers.modelName.toLowerCase()
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath("service/ServiceImpl.ejs"),
+      this.destinationPath("src/service/" + modelNameFile + "ServiceImpl.ts"),
+      {
+        modelName: this.answers.modelName,
+        modelVar: this.answers.modelName.toLowerCase()
       }
     );
   }
